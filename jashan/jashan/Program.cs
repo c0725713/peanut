8f6bae06b9316c3a7c79e0a95863c53ff5c1cdec
Jashandeep Singh Dhillon <725713@student.tor.loc>
2019-03-28 16:19:56 -0400

    final commit

---
+++ jashan/jashan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace jashan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            download();
+            Console.ReadLine();
+        }
+        static void download()
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                Console.WriteLine("Download Complete");
+            });
+        }
+    }
+}
